94145a11c19e3f565e4f5570d211d7476b984056
Iqbal Farhan Syuhada <iqbalfarhan08>
2023-05-23 14:44:41 +0800

    update sp

--- src/TelegramToolServiceProvider.php
+++ src/TelegramToolServiceProvider.php
@@ -8,16 +8,6 @@ class TelegramToolServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishConfig();
-    }
-
-    public function register()
-    {
-       //
-    }
-
-    protected function publishConfig()
-    {
         $this->publishes([
             __DIR__.'/config/telegramtools.php' => config_path('telegramtools.php'),
         ], 'telegramtool_config');
