710908df18d337f54649554ac031ebccab399e6d
Iqbal Farhan Syuhada <iqbalfarhan08>
2023-05-23 13:25:14 +0800

    update config

--- src/config/telegramtools.php
+++ src/config/telegramtools.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'link' => "https://api.telegram.org/bot".env("TELEGRAM_TOKEN")."/",
-    'default_chat_id' => '608751286'
+    'bot_token' => env('TELEGRAM_BOT_TOKEN'),
+    'default_chat_id' => env('TELEGRAM_DEFAULT_CHAT_ID'),
+    'link' => 'https://api.telegram.org/bot' . env('TELEGRAM_BOT_TOKEN') . '/',
+    'webhook_url' => env('TELEGRAM_WEBHOOK_URL'),
 ];
